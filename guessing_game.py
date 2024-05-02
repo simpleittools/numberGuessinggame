1047666f8df8fa5e8810fc37e0cd09b606adefeb
Ryan Mooney <wm.ryan.mooney@gmail.com>
2024-05-02 10:38:10 -0800

    add functionality to check if a valid number is entered when defining the low_num and high_num. Take the guesses list out of the start_game function, so it can store all the guesses. Add a game_guesses variable to store just the single game guess count. Create error messages to improve code readability. Add best_score (min) and worst score (max) variables. Change the mean, median, and mode to look against guesses variable to determine the total. set the mean_tries to a 2 digit rounded number (personal preference)

--- guessing_game.py
+++ guessing_game.py
@@ -8,32 +8,23 @@ import random
 import statistics
 
 
-# Create the start_game function.
-# Write your code inside this function.
 def start_game():
-    #   1. Display an intro/welcome message to the player.
-    print("Welcome to the Number Guessing Game by Simple IT Tools!")
-
-    # user can set the range of numbers
-    low_num = input("What number would you like to start the range from?\n ")
-    high_num = input("What number would you like to end the range from?\n ")
-
-    #   2. Store a random number as the answer/solution.
-    #  because the user is inputting data, this needs to be an int
+    # 2. Store a random number as the answer/solution.
+    # because the user is inputting data, this needs to be an int
     correct_number = random.randint(int(low_num), int(high_num))
 
     # 3. Continuously prompt the player for a guess. a. If the guess is greater than the solution, display to the
     # player "It's lower". b. If the guess is less than the solution, display to the player "It's higher". 4. Once
     # the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a
     # list.
-    guesses = []
+    game_guesses = 0
     while True:
         try:
             guess = int(input(f"Guess a number between {low_num} and {high_num}: "))
             # limit the user input
             if guess < int(low_num) or guess > int(high_num):
                 raise ValueError(f"Please enter a number between {low_num} and {high_num}.")
-            guesses.append(guess)
+            game_guesses += 1
             if guess > correct_number:
                 print("It's lower")
             elif guess < correct_number:
@@ -41,32 +32,62 @@ def start_game():
             else:
                 print("Got it!")
                 break
-        except ValueError as err:
-            print(err)
+        except ValueError:
+            print(input_error)
 
-    #   5. Display the following data to the player
-    #     a. How many attempts it took them to get the correct number in this game
-    tries = len(guesses)
-    #     b. The mean of the saved attempts list
-    mean_tries = statistics.mean(guesses)
-    #     c. The median of the saved attempts list
-    median_tries = statistics.median(guesses)
-    #     d. The mode of the saved attempts list
-    mode_tries = statistics.mode(guesses)
+    # 5. Display the following data to the player
+    #  a. How many attempts it took them to get the correct number in this game
+    tries = game_guesses
+
     print(f"Tries: {tries}")
-    print(f"Mean: {mean_tries}")
-    print(f"Median: {median_tries}")
-    print(f"Mode: {mode_tries}")
+
+    guesses.append(game_guesses)
+    # b. The mean of the saved attempts list
+    # Added: I don't like seeing long repeating numbers. So I am going to round this to two decimal places
+    mean_tries = round(statistics.mean(guesses), 2)
+    # c. The median of the saved attempts list
+    median_tries = statistics.median(guesses)
+    # d. The mode of the saved attempts list
+    mode_tries = statistics.mode(guesses)
+    best_score = min(guesses)
+    worst_score = max(guesses)
+    print(f"The best score is: {best_score}")
+    print(f"The worst score is: {worst_score}")
+    print(f"Average Score: {mean_tries}")
+    print(f"Middle Score: {median_tries}")
+    print(f"Most Common Score: {mode_tries}")
 
     #   6. Prompt the player to play again
     play_again = input("Do you want to play again? (yes/no): ")
     #     a. If they decide to play again, start the game loop over.
     if play_again.lower() == "yes" or play_again.lower() == "y":
+
         start_game()
     #     b. If they decide to quit, show them a goodbye message.
     else:
-        print("Goodbye!")
+        print("Thank you for playing. Goodbye.")
 
 
-# Kick off the program by calling the start_game function.
+# guesses is the total list of guesses for all rounds of the game
+guesses = []
+
+input_error = "You did not enter a valid whole number. Please enter a whole number."
+wrong_number_order = "The high number is less than the low number. Please enter a higher number."
+
+while True:
+    low_num = input("What number would you like to start the range from?\n ")
+    if low_num.isnumeric():
+        break
+    else:
+        print(input_error)
+
+while True:
+    high_num = input("What number would you like to start the range from?\n ")
+    if high_num < low_num:
+        print(wrong_number_order)
+    elif high_num.isnumeric():
+        break
+    else:
+        print(input_error)
+
 start_game()
